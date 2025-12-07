a91237b8e01e1b96529344da6032741cf9ba7ddf
vaibhavj8082 <142380352+vaibhavj8082@users.noreply.github.com>
2025-12-07 17:29:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
-variable "project_id" {}
-variable "region" {}
-variable "zone" {}
+variable "project_id" { type = string }
+variable "region"     { type = string, default = "us-central1" }
+variable "zone"       { type = string, default = "us-central1-a" }
+variable "instance_name" { type = string, default = "jenkins-gcp-vm" }
+variable "machine_type"  { type = string, default = "e2-medium" }
+variable "boot_image"    { type = string, default = "debian-cloud/debian-11" }
