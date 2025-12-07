ae24763c8f49a5c80e40bb7829b73cf31f7585f6
vaibhavj8082 <142380352+vaibhavj8082@users.noreply.github.com>
2025-12-07 17:29:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,36 @@
-provider "google" {
-  credentials = file("${path.module}/terraform.json")
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
+terraform {
+  required_version = ">= 1.0"
+
+  # Optional: GCS backend (recommended for team/shared state)
+  # backend "gcs" {
+  #   bucket = "your-terraform-state-bucket"
+  #   prefix = "terraform/state"
+  # }
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "jenkins-gcp-vm"
-  machine_type = "e2-micro"
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+  # The provider will use application default creds if GOOGLE_APPLICATION_CREDENTIALS env is set
+}
+
+resource "google_compute_instance" "vm" {
+  name         = var.instance_name
+  machine_type = var.machine_type
   zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.boot_image
+      size  = 10
     }
   }
 
   network_interface {
-    network       = "default"
-    access_config {}
+    network = "default"
+    access_config {} # gives external IP
   }
+
+  tags = ["jenkins-deployed"]
 }
